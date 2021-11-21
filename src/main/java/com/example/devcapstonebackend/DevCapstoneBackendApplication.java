a41b06c6621b0d31c5cb80d57f55a01c9077de9a
romsenkabomsen <50072487+romsenkabomsen@users.noreply.github.com>
2021-11-21 23:52:15 +0100

    initial commit

---
+++ src/main/java/com/example/devcapstonebackend/DevCapstoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.devcapstonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCapstoneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevCapstoneBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devcapstonebackend/DevCapstoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devcapstonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevCapstoneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
