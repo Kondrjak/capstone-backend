c270af0cfb9b2a77980b732a7686332647a6e0ef
romsenkabomsen <50072487+romsenkabomsen@users.noreply.github.com>
2021-11-26 01:02:22 +0100

    before merging other project

--- src/main/java/neuefische/capstone/backend/controller/HealthCheckController.java
+++ src/main/java/neuefische/capstone/backend/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package neuefische.capstone.backend;
+package neuefische.capstone.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
