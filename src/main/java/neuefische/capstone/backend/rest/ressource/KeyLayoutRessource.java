98025e8f560b4d6a664c3426a45eaf38573c1a9d
romsenkabomsen <50072487+romsenkabomsen@users.noreply.github.com>
2021-11-29 03:40:15 +0100

    enable cross origin for all rest ressources

--- src/main/java/neuefische/capstone/backend/rest/ressource/KeyLayoutRessource.java
+++ src/main/java/neuefische/capstone/backend/rest/ressource/KeyLayoutRessource.java
@@ -5,9 +5,11 @@ import neuefische.capstone.backend.rest.model.KeyLayout;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "key-layout", path = "key-layout")
 public interface KeyLayoutRessource extends PagingAndSortingRepository<KeyLayout, Long> {
     List<CodepointGroup> findByName(@Param("name") String name);
--- src/main/java/neuefische/capstone/backend/rest/ressource/TagRessource.java
+++ src/main/java/neuefische/capstone/backend/rest/ressource/TagRessource.java
@@ -1,13 +1,14 @@
 package neuefische.capstone.backend.rest.ressource;
 
-import neuefische.capstone.backend.rest.model.CodepointGroup;
 import neuefische.capstone.backend.rest.model.Tag;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "tag", path = "tag")
 public interface TagRessource extends PagingAndSortingRepository<Tag, Long> {
     List<Tag> findByName(@Param("name") String name);
